b4d4036eaab10384b3766edc631aa3791fe8919a
rabeeh-ta <rabeehchemnad@gmail.com>
2022-10-23 00:50:34 +0530

    done:html form

--- public/app.js
+++ public/app.js
@@ -1,20 +1,84 @@
 function NavBar() {
-  const [totalMailSend, setTotalMailSend] = React.useState({});
+  const [totalMailSend, setTotalMailSend] = React.useState();
 
   fetch('https://pace-mail-service.onrender.com/mailsStat')
     .then((res) => res.json())
-    .then((data) => setTotalMailSend(data));
+    .then((data) => {
+      setTotalMailSend(data.count);
+    });
+
   return (
     <nav>
       <img src="./postbox.png" />
       <h2> Pace Post Service</h2>
-      <p>Total Mails Send {totalMailSend.count}</p>
+      <p>Total Mails Send {totalMailSend}</p>
     </nav>
   );
 }
 
 function LetterBody() {
-  return <section className="letter"></section>;
+  const [mailData, setMailData] = React.useState({
+    toEmail: '',
+    subject: '',
+    message: '',
+  });
+
+  function handleChange(e) {
+    console.log(e.target.name, e.target.value);
+    setMailData({
+      ...mailData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function sendMail() {
+    console.log(mailData);
+  }
+
+  return (
+    <section className="letter">
+      <form>
+        <label className="form--label">To</label>
+        <input
+          className="form--input"
+          type="text"
+          name="toEmail"
+          placeholder="username@example.com"
+          onChange={handleChange}
+        />
+
+        <br />
+        <label className="form--label">Subject</label>
+        <input
+          className="form--input"
+          type="text"
+          name="subject"
+          placeholder=""
+          onChange={handleChange}
+        />
+
+        <br />
+        <label className="form--label">Subject</label>
+        <textarea
+          className="form--textarea"
+          name="message"
+          id=""
+          cols="30"
+          rows="10"
+          onChange={handleChange}
+        ></textarea>
+
+        <br />
+        <label className="form--label">Attachment</label>
+        <input type="file" />
+
+        <br />
+        <button type="button" onClick={sendMail}>
+          send
+        </button>
+      </form>
+    </section>
+  );
 }
 
 function App() {
