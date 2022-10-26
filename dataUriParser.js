bacfee9ad492c86289f370e6ed8a9dc79403966b
rabeeh-ta <rabeehchemnad@gmail.com>
2022-10-26 18:23:03 +0530

    parse the dataURI and return info as json

---
+++ dataUriParser.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const dataUri = fs.readFileSync('./imgdot.txt');
+
+function dataUriParser(uri) {
+  const [meta, data] = uri.split(',');
+  const fileType = meta.match(':([^;]*);')[1].split('/');
+  const encoding = meta.split(';')[1];
+
+  return {
+    meta: meta,
+    data: data,
+    type: fileType,
+    fileFormat: fileType[1],
+    encoding: encoding,
+  };
+}
+
+module.exports = dataUriParser;
+
+// console.log(dataUriParser(dataUri.toString()));
